a056daae329c8de562b05206fbf4d74da120c79e
Eriknpy <e.business@tuta.io>
2024-02-14 14:31:53 +0100

    Add file reader function
    + get_user_story_content(file_path: str) -> str

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from user_stories import user_story_1
+import os
 
 
 def main():
-    user_story_1()
+    directory = "./user_story_inputs/"
+    file_name = f"{os.listdir(directory)[0]}"
+    file_path = os.path.join(directory, file_name)
+    input_file = get_user_story_content(file_path)
+    user_story_1(input_file)
 
+
+def get_user_story_content(file_path: str) -> str:
+    with open(file_path, 'r') as f:
+        return f.read()
 
 
 if __name__ == "__main__":
