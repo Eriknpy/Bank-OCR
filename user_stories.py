5da25d0b88823e8cd48081605281e132b0483139
Eriknpy <e.business@tuta.io>
2024-02-14 13:56:23 +0100

    Create user_stories
    + Add first user story description

---
+++ user_stories.py
@@ -0,0 +1,21 @@
+"""
+Task description:
+    Each entry is 4 lines long, and each line has 27 characters.
+    The first 3 lines of each entry contain an account number written using pipes and underscores,
+    and the fourth line is blank. Each account number should have 9 digits, all of which should be in the range 0-9.
+    A normal file contains around 500 entries.
+
+Task bullet points:
+    - Vertical 4 lines long - the first 3 lines contain an account number and the 4th is blank
+    - Each account number should have 9 digits - range 0-9
+    - Horizontal 27 chars long
+    - A normal file contains around 500 entries
+
+Task goal:
+    - Your first task is to write a program that can take this file and parse it into actual account numbers.
+"""
+
+
+def user_story_1():
+    print(f"Hello from user_story_1!")
+
